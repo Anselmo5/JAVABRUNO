c15daafda1cc8d12467b00847746763a83b59727
Anselmo Henrique <100934388+Anselmo5@users.noreply.github.com>
2023-03-31 09:53:16 -0300

    anselmo

---
+++ JAVAPOO/Main.java
@@ -0,0 +1,35 @@
+// package JAVAPOO;
+
+// public class Main{
+
+//     public static void main(String[] args){
+//         System.out.println("Hello, work");
+//     }
+// }
+
+// public class Main{
+
+	
+	
+	
+//     public static void main(String[] args){
+//     	Animal bil = new Animal();
+//     	bil.nome = "bil souza";
+//     	bil.peso = 2.5;
+//         System.out.println(bil);
+        
+    
+//     }
+// }
+
+package JAVAPOO;
+
+public class Main{
+    public static void main(String[] args){
+    	animal bil = new animal("bil", 2.5);
+        System.out.println(bil);
+        
+       
+    }
+}
+
---
+++ JAVAPOO/animal.java
@@ -0,0 +1,43 @@
+package JAVAPOO;
+
+
+public class animal {
+	private String nome;
+	private double peso;
+    public animal(String nome, double peso) {
+		this.nome = nome;
+		this.peso = peso;
+	}
+	
+	 public void Imprimir() {
+ 		System.out.println(
+ 				"Nome: " + this.nome +
+ 				"\nPeso: "+ this.peso);
+     }
+}
+
+
+
+//public class animal{
+
+   // public class Animal {
+       // public String nome;
+       // public double peso;
+        
+    //}
+	
+	// let nome = 'nome dele'
+	// String nome = "bil";
+    // String sobrenome = "souza";
+    // char letra = 'B';
+	
+    // public static void main(String[] args)  {
+        // String nome = "bil";
+        // String sobrenome = "souza";
+        // animal bil = new animal();
+        // System.out.println();
+         // mesmo sistema do console.log
+    //}
+
+
+//}
